2d733da87bc713b4689f81b8cdb668bfdbc52cbf
Israel Amaral Linhares Terra <israel.terra@take.net>
2022-05-12 16:07:43 -0300

    ex3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,15 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+var buttons = document.querySelectorAll(".botao-expandir-retrair")
+
+buttons.forEach((btn)=>{
+    btn.addEventListener('click',showMoreInfo)
+})
+
+function showMoreInfo(evt){
+    console.log("Aqui");
+    let btn = evt.currentTarget
+    let p = btn.closest('p');
+    p.classList.toggle('expandido')
+
+}
